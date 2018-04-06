685b8cfb136f4667c4c80eb0d329e2179090c9ec
Joosep Penjam <joosep98@tlu.ee>
2018-04-06 15:35:58 +0300

    parandus

--- options.js
+++ options.js
@@ -1,4 +1,6 @@
 const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1']
+
+
   function constructOptions(kButtonColors) {
     for (let item of kButtonColors) {
       let button = document.createElement('button');
@@ -8,7 +10,7 @@ const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1']
           console.log('color is ' + item);
         })
       });
-      page.appendChild(button);
+      document.getElementById("buttonDiv").appendChild(button);
     }
   }
   constructOptions(kButtonColors);
